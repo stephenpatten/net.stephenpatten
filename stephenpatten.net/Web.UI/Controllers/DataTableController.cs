6f62b27b88a886194968b071a7b86bdfa2cc6202
stephenpatten:Adv41722 <stephenpatten@gmail.com>
2012-02-17 21:36:52 -0800

    working on the DT example

---
+++ stephenpatten.net/Web.UI/Controllers/DataTableController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.UI.Controllers
+{
+    public class DataTableController : Controller
+    {
+        //
+        // GET: /DataTable/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
