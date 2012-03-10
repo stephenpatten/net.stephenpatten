4a09ec1befd4708f2293464af62774d80526cab9
stephenpatten:Adv41722 <stephenpatten@gmail.com>
2012-03-10 07:36:25 -0800

    working on the Bootstrap 2 example

---
+++ stephenpatten.net/Web.UI/Areas/BootstrapLite/BootstrapLiteAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Web.UI.Areas.BootstrapLite
+{
+    public class BootstrapLiteAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "BootstrapLite";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "BootstrapLite_default",
+                "BootstrapLite/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ stephenpatten.net/Web.UI/Areas/BootstrapLite/Controllers/SteppingOutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.UI.Areas.BootstrapLite.Controllers
+{
+    //http://webdesign.tutsplus.com/tutorials/htmlcss-tutorials/stepping-out-with-bootstrap-from-twitter/#grid_link
+    //http://designshack.net/articles/css/twitter-bootstrap-2-bootstrap-goes-responsive/ 
+
+    public class SteppingOutController : Controller
+    {
+        //
+        // GET: /BootstrapLite/SteppingOut/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
