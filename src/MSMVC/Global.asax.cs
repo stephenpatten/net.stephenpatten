2054a13c13efc0b74f74ebfe797dd4333c07f477
stephenpatten:Adv41722 <stephenpatten@gmail.com>
2013-03-17 12:11:16 -0700

    bootstrap and sample for mvc4

--- src/MSMVC/Global.asax.cs
+++ src/MSMVC/Global.asax.cs
@@ -22,6 +22,8 @@ namespace MSMVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            BootstrapSupport.BootstrapBundleConfig.RegisterBundles(System.Web.Optimization.BundleTable.Bundles);
+            BootstrapMvcSample.ExampleLayoutsRouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
